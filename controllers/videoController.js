f90b166f7c152ab2457d6b48e595fa2fd7a42860
Simjaeryun <tlawofbs123@naver.com>
2020-12-02 23:05:27 +0900

    mongo
    mogno

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,32 +1,95 @@
 //global
 import routes from "../routes";
-export const home = (req, res) => {
-  res.render("home", { pageTitle: "Home", videos });
+import Video from "../models/Video";
+
+//HOME
+export const home = async (req, res) => {
+  try {
+    const videos = await Video.find({});
+    console.log(videos);
+    res.render("home", { pageTitle: "Home", videos });
+  } catch (error) {
+    console.log(error);
+    res.render("home", { pageTitle: "Home", videos: [] });
+  }
 };
+
+// SEARCH
 export const search = (req, res) => {
   const {
     query: { term: searchingBy },
   } = req;
   res.render("search", { pageTitle: "Search", searchingBy, videos });
 };
-//video
+
+//UPLOAD
 export const getUpload = (req, res) => {
   res.render("upload", { pageTitle: "upload" });
 };
-export const postUpload = (req, res) => {
+export const postUpload = async (req, res) => {
   const {
-    body: { file, title, description },
+    body: { title, description },
+    file: { path },
   } = req;
-  // 할일 업로드하는여상 저장
-  res.redirect(routes.videoDetail(321321));
+  const newVideo = await Video.create({
+    fileUrl: path,
+    title: title,
+    description: description,
+  });
+  console.log(newVideo);
+  res.redirect(routes.videoDetail(newVideo.id));
 };
 
-export const videoDetail = (req, res) => {
-  res.render("videoDetail", { pageTitle: "Video Detail" });
+//VIDEODETAIL
+export const videoDetail = async (req, res) => {
+  const {
+    params: { id },
+  } = req;
+  try {
+    const video = await Video.findById(id);
+    console.log(video);
+    res.render("videoDetail", { pageTitle: video.title, video: video });
+  } catch (error) {
+    console.log(Error);
+    res.redirect(routes.home);
+  }
 };
-export const editVideo = (req, res) => {
-  res.render("editVideo", { pageTitle: "Edit Video" });
+
+// EDITVIDEO
+export const getEditVideo = async (req, res) => {
+  const {
+    params: { id },
+  } = req;
+  try {
+    const video = await Video.findById(id);
+    res.render("editVideo", { pageTitle: `Edit ${video.title}`, video: video });
+  } catch (error) {
+    res.redirect(routes.home);
+  }
 };
-export const deleteVideo = (req, res) => {
-  res.render("deleteVideo", { pageTitle: "Delete Video" });
+
+export const postEditVideo = async (req, res) => {
+  const {
+    params: { id },
+    body: { title, description },
+  } = req;
+  try {
+    await Video.findOneAndUpdate(
+      { _id: id },
+      { title: title, description: description }
+    );
+  } catch (error) {
+    res.redirect(routes.home);
+  }
+};
+
+//DELETEVIDEO
+export const deleteVideo = async (req, res) => {
+  const {
+    params: { id },
+  } = req;
+  try {
+    await video.findOneAndRemove({ _id: id });
+  } catch (error) {}
+  res.redirect(routes.home);
 };
--- routers/videoRouter.js
+++ routers/videoRouter.js
@@ -5,16 +5,25 @@ import {
   postUpload,
   videoDetail,
   deleteVideo,
-  editVideo,
+  getEditVideo,
+  postEditVideo,
 } from "../controllers/videoController";
+import { uploadVideo } from "../middlewares/middlewares";
 
 const videoRouter = express.Router();
 
+//UPLOAD
 videoRouter.get(routes.upload, getUpload);
-videoRouter.post(routes.upload, postUpload);
+videoRouter.post(routes.upload, uploadVideo, postUpload);
 
+//VIDEO DETAIL
 videoRouter.get(routes.videoDetail(), videoDetail); //임의수정
-videoRouter.get(routes.editVideo, editVideo);
-videoRouter.get(routes.deleteVideo, deleteVideo);
+
+// Edit VIDEO
+videoRouter.get(routes.editVideo(), getEditVideo);
+videoRouter.post(routes.editVideo(), postEditVideo);
+
+//DELETE VIDEO
+videoRouter.get(routes.deleteVideo(), deleteVideo);
 
 export default videoRouter;
